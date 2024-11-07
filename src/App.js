c43e633ec519658fef7847fa53dca01ef2eacf09
Mouni1526 <seerammounika2002@gmail.com>
2024-11-07 17:11:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function countDecrement() {
+    setCount(count - 1);
+  }
+  function countIncrement() {
+    setCount(count + 1);
+  }
+  return (
+    <div className="App">
+      <button onClick={countDecrement}>Decrement</button>
+      <span>{count}</span>
+      <button onClick={countIncrement}>Increment</button>
+    </div>
+  );
+}
+
+export default App;
